5b47d3c6fa1d884744cead805adb22d8d294c339
jeandavidt <jeandavidt@gmail.com>
2020-02-07 18:11:38 -0500

    localization is fixed

--- AnaPro_37.py
+++ AnaPro_37.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import os
 
-import pymssql
 from collections import namedtuple
 from datetime import datetime as dt
 import re
@@ -12,6 +11,31 @@ from sqlalchemy import create_engine
 database_name = 'dateaubaseSandbox'
 local_server = r'GCI-PR-DATEAU01\DATEAUBASE'
 remote_server = '10.10.10.10'
+path = "sample_files"
+with open('login.txt') as f:
+    username = f.readline().strip()
+    password = f.readline().strip()
+
+
+def connect_local(server, database):
+    try:
+        engine = create_engine(f'mssql://{local_server}/{database_name}?trusted_connection=yes', fast_executemany=True)
+    except Exception as e:
+        print("local connection error")
+        print(e)
+        return None
+    return engine
+
+
+def connect_remote(server, database):
+    try:
+        engine = create_engine(f'mssql+pyodbc://jeandavidt:koopa6425@{remote_server}:1433/{database_name}?driver=ODBC+Driver+13+for+SQL+Server', fast_executemany=True)
+        print('remote connection ok')
+    except Exception as e:
+        print("remote connection error")
+        print(e)
+        return None
+    return engine
 
 
 def get_last(db_engine):
@@ -20,44 +44,6 @@ def get_last(db_engine):
     Record = namedtuple('record', result.keys())
     records = [Record(*r) for r in result.fetchall()]
     return records[0]
-
-
-def connect_local(server, database):
-    try:
-        cnx = pymssql.connect(server=server, database=database)
-        curs = cnx.cursor()
-        print("conn ok")
-        return cnx, curs
-    except Exception as e:
-        print("local connection error")
-        print(e)
-        return None, None
-
-
-'''def connect_remote(server, database, usr, pwd):
-    config = dict(
-        server=server,  # change this to your SQL Server hostname or IP address
-        port=1433,  # change this to your SQL Server port number [1433 is the default]
-        database=database,
-        username=usr,
-        password=pwd)
-    conn_str = (
-        'SERVER={server},{port};'
-        + 'DATABASE={database};'
-        + 'UID={username};'
-        + 'PWD={password}')
-    try:
-        conn = pyodbc.connect(
-            r'DRIVER={ODBC Driver 13 for SQL Server};'
-            + conn_str.format(**config)
-        )
-        cursor = conn.cursor()
-        print('remote connection ok')
-        return cursor, conn
-    except Exception as e:
-        print("remote connection error")
-        print(e)
-        return None, None'''
 
 
 def get_par_files(path):
@@ -97,7 +83,7 @@ def read_par(f_name, db_engine):
     df.fillna(0, inplace=True)
 
     # Convert the datetime strings into epoch time
-    df['Datetime'] = pd.to_datetime(df['Datetime'], format='%Y.%m.%d  %H:%M:%S').astype(np.int64) // 10 ** 9
+    df['Datetime'] = pd.to_datetime(df['Datetime'], format='%Y.%m.%d  %H:%M:%S').dt.tz_localize('US/Eastern').astype(np.int64) // 10 ** 9
 
     # Get the last ID from the database
     last_ID, last_Timestamp = get_last(engine)
@@ -149,6 +135,7 @@ def read_par(f_name, db_engine):
     # reorder columns
     df = df[['Value_ID', 'Value', 'Number_of_experiment', 'Metadata_ID', 'Comment_ID', 'Timestamp']]
 
+    # And return result!
     return df
 
 
@@ -157,56 +144,56 @@ def send_to_db(df, db_engine):
     df.to_sql('value', con=db_engine, if_exists='append', index=False)
     return None
 
-# This snippet is needed to run the script on the dateaubase computer
-# cursor, conn = connect_local(local_server, database_name)
-# if conn:
-#    engine = create_engine(f'mssql://{local_server}/{database_name}?trusted_connection=yes')
-# if engine:
-#    print('engine is running')
 
-# This snippet is needed for remote testing only
+# _________Main Function__________
 
 
-with open('login.txt') as f:
-    username = f.readline().strip()
-    password = f.readline().strip()
-# cursor, conn = connect_remote(remote_server, database_name, username, password)
-engine = create_engine(
-    f'mssql+pyodbc://{username}:{password}@{remote_server}:1433/{database_name}?driver=ODBC+Driver+13+for+SQL+Server',
-    fast_executemany=True
-)
-if engine:
-    print('engine is running')
+def main(engine):
+    # Find the .par files on the path
+    index, file_list = get_par_files(path)
+
+    # get the latest timestamp on the dateaubase
+    _, last_Timestamp = get_last(engine)
+
+    # Convert the Timestamp in DateTime format
+    db_last_time = dt.fromtimestamp(last_Timestamp)
+
+    # loop to check all the files for data more recent than the last value in the db
+    for idx, file in enumerate(file_list):
+        with open(file, 'r') as f:
+            first_point_line = f.read().splitlines()[2]
+            first_time_string = re.split(r'\t', first_point_line)[0]
+            file_first_time = dt.strptime(first_time_string, '%Y.%m.%d  %H:%M:%S')
+        # if a file is more recent than the last data => change the index to begin the import with the
+        # file just before this one
+        if file_first_time > db_last_time:
+            index = max(idx - 1, 0)
+            break
+
+    new_records = 0
+    for file in file_list[index:]:
+        new_data = read_par(file, engine)
+        if new_data is not None:
+            send_to_db(new_data, engine)
+            new_records += len(new_data)
+
+    print(f"Added {new_records} rows to {database_name}")
 
 
-path = "sample_files"
-index, file_list = get_par_files(path)
+# ________Main Script_________
+'''try:
+    engine = connect_local(local_server, database_name)
+    print('local connection engine is running')
+except Exception:
+    print(e)'''
 
-_, last_Timestamp = get_last(engine)
+try:
+    engine = connect_remote(remote_server, database_name)
+    print('remote connection engine is running')
+except Exception as e:
+    print(e)
 
-# Convert the Timestamp in DateTime format
-db_last_time = dt.fromtimestamp(last_Timestamp)
-
-# loop to check all the files for data more recent than the last value in the db
-for idx, file in enumerate(file_list):
-    with open(file, 'r') as f:
-        first_point_line = f.read().splitlines()[2]
-        first_time_string = re.split(r'\t', first_point_line)[0]
-        file_first_time = dt.strptime(first_time_string, '%Y.%m.%d  %H:%M:%S')
-    # if a file is more recent than the last data => change the index to begin the import with the
-    # file just before this one
-    if file_first_time > db_last_time:
-        index = max(idx - 1, 0)
-        break
-
-new_records = 0
-for file in file_list[index:]:
-    new_data = read_par(file, engine)
-    if new_data is not None:
-        send_to_db(new_data, engine)
-        new_records += len(new_data)
-
-print(f"Added {new_records} rows to {database_name}")
-
-# Close the connexion to the DB
-engine.dispose()
+try:
+    main(engine)
+finally:
+    engine.dispose()
